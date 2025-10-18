baeea0d42e241545aee1b5fe7fc43738b04c5de4
appleuser <usesession@gmail.com>
2025-10-18 16:03:41 +0300

    backend/init: инициализация

---
+++ backend/src/main.ts
@@ -0,0 +1,15 @@
+import 'module-alias/register';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+async function main() {
+  const app = express();
+  app.use(express.json());
+  app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
+
+  app.listen(() => console.log('Server started on', process.env.PORT));
+}
+
+main();
