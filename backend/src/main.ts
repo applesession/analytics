6d53e8d136f4e816149fee26000b237ef1175731
appleuser <usesession@gmail.com>
2025-10-19 02:12:50 +0300

    chore

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function main() {
   const app = express();
   app.use(express.json());
   app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
-  app.use('/api', routes);
+  app.use('/', routes);
 
   app.listen(process.env.PORT, () => console.log('Server started on', process.env.PORT));
 }
